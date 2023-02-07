1d7d708030446b6bf47a7785086537cb5d073e77
nRibasMercau <nicolas.ribasmercau@gmail.com>
2023-02-07 20:42:32 +0100

    added cors

--- part3/notesapp/index.js
+++ part3/notesapp/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -69,7 +71,7 @@ app.post('/api/notes', (request, response) => {
     }
 
     notes = notes.concat(note)
-    .
+    
     response.json(note)
 })
 
@@ -79,8 +81,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+        })
 
